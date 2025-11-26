04d2d800914f82124771e50e8b92f5f67270cee1
ECE CALISKAN <ececaliskan@ECEs-MacBook-Pro.local>
2025-11-26 20:31:31 +0000

    feat: Add Appbar component and integrate into app layout

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './component/auth/login/login';
+import { Appbar } from './component/templates/appbar/appbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet, Login, Appbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
