df98fb649deb78f4fa10035f1cf441bc051788d9
eceCaliskan <ece98caliskan@gmail.com>
2025-11-30 22:20:08 +0000

    add: connect Register component with AuthService to register user

--- frontend/src/app/component/auth/register/register.ts
+++ frontend/src/app/component/auth/register/register.ts
@@ -8,10 +8,21 @@ import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
 import { MaterialModule } from '../../../shared/material-import';
 import { LoginFormType } from '../../../model/form-model';
+import { AuthService } from '../../../service/auth/auth-service';
+
 
 @Component({
   selector: 'app-register',
-  imports: [MaterialModule, MatFormFieldModule, MatFormFieldModule, MatLabel, MatInputModule, MatIconModule, MatButtonModule, MatCard],
+  imports: [
+    MaterialModule,
+    MatFormFieldModule,
+    MatFormFieldModule,
+    MatLabel,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCard,
+  ],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
@@ -21,28 +32,61 @@ export class Register {
   imagePath: string;
   buttonList: LoginFormType[];
   userRegisterFormGroup: FormGroup;
-  user:{email: string, password: string, role: string};
+  user: { email: string; password: string; role: string };
 
-    constructor(private router: Router){
-      this.formName = 'Register';
-      this.buttonText = 'Register';
-      this.userRegisterFormGroup = new FormGroup({'emailInputCtrl': new FormControl(''), 'passwordInputCtrl': new FormControl(''), 'roleInputCtrl': new FormControl('')});
-      this.buttonList = [
-        {name: 'email', icon: 'email', placeholder: 'Enter your email', inputFormGroup: this.userRegisterFormGroup, inputControl: 'emailInputCtrl'},
-        {name: 'password', icon: 'lock', placeholder: 'Enter your password', inputFormGroup: this.userRegisterFormGroup, inputControl: 'passwordInputCtrl'},
-        {name: 'role', icon: 'lock', placeholder: 'Enter your role', inputFormGroup: this.userRegisterFormGroup, inputControl: 'roleInputCtrl'}];
-      this.imagePath = 'assets/images/image.png';
-      this.user = {email: '', password: '', role: ''};
-      
-    }
-    
-    /**
-     * Handles the register action and navigates to the document list page.
-     */
-    onRegister() {
-      this.user.email = this.userRegisterFormGroup.value.emailInputCtrl;
-      this.user.password = this.userRegisterFormGroup.value.passwordInputCtrl;
-      this.user.role = this.userRegisterFormGroup.value.roleInputCtrl;
-      console.log(this.user);
-      this.router.navigate(['/list']);}
+  constructor(private router: Router, private authService: AuthService) {
+    this.formName = 'Register';
+    this.buttonText = 'Register';
+    this.userRegisterFormGroup = new FormGroup({
+      emailInputCtrl: new FormControl(''),
+      passwordInputCtrl: new FormControl(''),
+      roleInputCtrl: new FormControl(''),
+    });
+    this.buttonList = [
+      {
+        name: 'email',
+        icon: 'email',
+        placeholder: 'Enter your email',
+        inputFormGroup: this.userRegisterFormGroup,
+        inputControl: 'emailInputCtrl',
+      },
+      {
+        name: 'password',
+        icon: 'lock',
+        placeholder: 'Enter your password',
+        inputFormGroup: this.userRegisterFormGroup,
+        inputControl: 'passwordInputCtrl',
+      },
+      {
+        name: 'role',
+        icon: 'lock',
+        placeholder: 'Enter your role',
+        inputFormGroup: this.userRegisterFormGroup,
+        inputControl: 'roleInputCtrl',
+      },
+    ];
+    this.imagePath = 'assets/images/image.png';
+    this.user = { email: '', password: '', role: '' };
+  }
+
+  /**
+   * Handles the register action and navigates to the document list page.
+   */
+  onRegister() {
+    this.user.email = this.userRegisterFormGroup.value.emailInputCtrl;
+    this.user.password = this.userRegisterFormGroup.value.passwordInputCtrl;
+    const role = this.userRegisterFormGroup.value.roleInputCtrl;
+    this.authService.firebaseSignUp(this.user, role).then((userCredential) => {
+        const user = userCredential.user;
+        console.log('User created:', user.uid);
+        // You can now send user.uid to your backend to assign roles
+        // example:
+        // this.http.post("/assign-role", { uid: user.uid })
+      })
+      .catch((error) => {
+        console.error('Error during sign up:', error);
+      });
+  }
+  
+  
 }
