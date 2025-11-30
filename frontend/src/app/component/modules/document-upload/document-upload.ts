cafe0d7d7f96f04317e913a3da462560a680167d
eceCaliskan <ece98caliskan@gmail.com>
2025-11-30 11:44:57 +0000

    feat: Refactor document upload forms and enhance file handling logic

--- frontend/src/app/component/modules/document-upload/document-upload.ts
+++ frontend/src/app/component/modules/document-upload/document-upload.ts
@@ -16,20 +16,59 @@ import { MaterialModule } from '../../../shared/material-import';
 export class DocumentUpload {
   
   private _formBuilder: FormBuilder;
-  firstFormGroup: FormGroup;
-  secondFormGroup : FormGroup;
+  projectNameFormGroup!: FormGroup;
+  regionPreferenceFormGroup!: FormGroup;
+  documentFormGroup!: FormGroup;
   sectionLabel: string 
+  selectedFile: File | null = null;
 
   constructor(private router: Router) {
     this._formBuilder = inject(FormBuilder);
     this.sectionLabel = 'Document Upload';
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required],
+    this.setUpForms();
+  }
+
+  /**
+   * Sets up the form groups for the document upload process.
+   * @returns void
+   */
+  setUpForms(): void {
+    this.projectNameFormGroup = this._formBuilder.group({
+      projectNameCtrl: ['', Validators.required],
     });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required],
+    this.regionPreferenceFormGroup = this._formBuilder.group({
+      regionPreferenceCtrl: ['']
+    });
+    this.documentFormGroup = this._formBuilder.group({
+      documentCtrl: ['', Validators.required],
     });
   }
+
+/**
+ * Handles the file upload process.
+ * @returns void
+ */
+  uploadFile(): void {
+    console.log(this.regionPreferenceFormGroup.value.regionPreferenceCtrl);
+    console.log(this.projectNameFormGroup.value.projectNameCtrl);
+    console.log(typeof this.documentFormGroup.value.documentCtrl);
+    console.log(this.documentFormGroup);
+    console.log(this.selectedFile);
+  }
+
+/**
+ * Handles the file selection event.
+ * @param event The file selection event.
+ * @returns void
+ */
+onFileSelected(event: Event): void {
+  const input = event.target as HTMLInputElement;
+  const file = input.files?.[0] ?? null;
+  if (!file) return;
+  this.documentFormGroup.patchValue({ documentCtrl: file });
+  this.documentFormGroup.get('documentCtrl')?.updateValueAndValidity();
+  this.selectedFile = file;
+}
 }
 
 
