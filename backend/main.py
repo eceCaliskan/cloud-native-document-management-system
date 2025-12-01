914d974c6649b651a6d0f4c9f4ac57f69b9c1341
eceCaliskan <ece98caliskan@gmail.com>
2025-12-01 14:28:35 +0000

    feat: implement role setting endpoint for firebase auth

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+import firebase_admin
+from firebase_admin import credentials, auth
+
+cred = credentials.Certificate("carbonandcostawaredms-firebase-adminsdk-fbsvc-ee7a06d7cb.json")
+firebase_admin.initialize_app(cred)
 
 app = FastAPI(title="FastAPI Backend")
 
@@ -6,3 +11,8 @@ app = FastAPI(title="FastAPI Backend")
 async def read_root():
     return {"message": "Hello, FastAPI!"}
 
+
+@app.get("/user_role")
+async def get_user_role(uuid: str, user_role: str):
+    auth.set_custom_user_claims(uuid, {"admin": True} if user_role == "admin" else {"admin": False})
+    return {"message": f"User role is {user_role} and UUID is {uuid}"}
